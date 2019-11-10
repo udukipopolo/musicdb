6266a41ecf0c8487fe5b9058601239c5c6f77362
udukipopolo <udukipopolo@gmail.com>
2019-11-10 19:01:16 +0900

    開発

--- resources/views/manage/artist/create.blade.php
+++ resources/views/manage/artist/create.blade.php
@@ -10,16 +10,17 @@
             <div class="card-body">
                 {{ Form::open(['route'=>['manage.artist.store'], 'method'=>'POST', 'class'=>'']) }}
                 <div class="form-group row">
-                    {{ Form::label('name', ['class'=>'col-form-label col-md-4'])}}
+                    {{ Form::label('name', 'アーティスト名', ['class'=>'col-form-label col-md-4'])}}
                     <div class="col-md-8">
                         {{ Form::text('name', null, ['class'=>'form-control']) }}
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    {{ Form::label('belonging', ['class'=>'col-form-label col-md-4'])}}
+                    {{ Form::label('belonging', '所属事務所', ['class'=>'col-form-label col-md-4'])}}
                     <div class="col-md-8">
                         {{ Form::textarea('belonging', null, ['class'=>'form-control', 'cols'=>'6']) }}
+                        <p class="help-block">※複数ある場合、もしくは変更する場合は改行で追記してください。</p>
                     </div>
                 </div>
 

ef9eea9a1e29ebad73792a482d37016f30c3e939
udukipopolo <udukipopolo@gmail.com>
2019-11-10 18:25:15 +0900

    開発

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,11 +55,9 @@
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     {{ Html::linkRoute('manage.album.index', 'アルバム管理', [], ['class'=>'dropdown-item']) }}
+                                    {{ Html::linkRoute('manage.artist.index', 'アーティスト管理', [], ['class'=>'dropdown-item']) }}
                                 </div>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        {{ Html::linkRoute('manage.artist.index', 'アーティスト管理', [], ['class'=>'dropdown-item']) }}
-                                    </div>
-                                </li>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>

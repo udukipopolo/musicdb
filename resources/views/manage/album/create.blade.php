fc002ed4663926bc23130d52cf136f7213409f50
udukipopolo <udukipopolo@gmail.com>
2019-11-11 13:36:56 +0900

    test

--- resources/views/manage/album/create.blade.php
+++ resources/views/manage/album/create.blade.php
@@ -6,7 +6,7 @@
 <script>
 $(document).ready(function(){
     $('.js-select2').select2()
-        .on('change', function(e) {
+        .on('change.select2', function(e) {
             var data = e.params.data;
             console.log(data);
         });

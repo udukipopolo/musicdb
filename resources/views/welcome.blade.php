f63f78eeff77bcdc08d9c668003edbb1ddcd05e7
udukipopolo <udukipopolo@gmail.com>
2019-11-11 12:43:06 +0900

    開発中

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,15 +67,14 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
+                    <a href="{{ url('/home') }}">Home</a>
+                    @guest
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
-                    @endauth
+                    @endguest
                 </div>
             @endif
 
@@ -85,7 +84,7 @@
                 </div>
 
                 <div class="links">
-                    <a href=""></a>
+                    <a href="/home">HOME</a>
                 </div>
             </div>
         </div>

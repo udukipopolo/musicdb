8ade4b66a9ddf6b5266e63f1144f871ce1861c3d
udukipopolo <udukipopolo@gmail.com>
2019-11-13 02:02:44 +0900

    dev

--- resources/views/manage/album/music/edit.blade.php
+++ resources/views/manage/album/music/edit.blade.php
@@ -133,7 +133,7 @@ $(document).ready(function(){
                                     <td>
                                         {{ Form::select("add_artist_id[{$no}]", [''=>'新規登録']+$artists->toArray(), old('add_artist_id.'.$no), ['class'=>'form-control js-select2'.ViewUtil::hasErrorClass($errors, 'add_artist_id.'.$no), 'id'=>'add_artist_'.$no]) }}
                                         {{ Form::text("add_artist_name[{$no}]", old('add_artist_name.'.$no), ['class'=>'form-control'.ViewUtil::hasErrorClass($errors, 'add_artist_name.'.$no), 'id'=>"add_artist_{$no}_name"]) }}
-                                        @include('layouts.parts.error_message', ['key'=>'add_artist_id.'.$no])
+                                        @include('layouts.parts.error_message', ['key'=>'add_artist_name.'.$no])
                                     </td>
                                     <td>
                                         {{ Form::text("add_part_name[{$no}]", old('add_part_name.'.$no), ['class'=>'form-control'.ViewUtil::hasErrorClass($errors, 'add_part_name.'.$no)]) }}

da44326135bcd736b4d2af744329f6a5fb56beab
udukipopolo <udukipopolo@gmail.com>
2019-11-13 10:44:25 +0900

    dev

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">楽曲検索</div>
 
@@ -33,7 +33,7 @@
             </div>
         </div>
 
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">アーティスト検索</div>
 

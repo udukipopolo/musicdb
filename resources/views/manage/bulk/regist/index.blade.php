8667ffad6d22992ccf0e54555ee0c9af3008ac59
udukipopolo <udukipopolo@gmail.com>
2019-11-15 12:15:11 +0900

    csvアップロード

--- resources/views/manage/bulk/regist/index.blade.php
+++ resources/views/manage/bulk/regist/index.blade.php
@@ -15,9 +15,9 @@
                     {{ Form::open(['route'=>['manage.bulk.regist.csv'], 'method'=>'POST', 'class'=>'form-inline']) }}
                         <div class="form-group">
                         {{ Form::file('csv_file', ['class'=>''.ViewUtil::hasErrorClass($errors, 'csv_file')]) }}
+                        @include('layouts.parts.error_message', ['key'=>'csv_file'])
                         </div>
                         {{ Form::submit('送信', ['class'=>'form-control']) }}
-                        @include('layouts.parts.error_message', ['key'=>'csv_file'])
                     {{ Form::close() }}
                 </div>
             </div>

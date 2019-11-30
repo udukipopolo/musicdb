2fa62535814217d15dce9602a6e6ebf206f63ab4
udukipopolo <udukipopolo@gmail.com>
2019-11-30 16:44:32 +0900

    gss一括

--- app/Http/Controllers/ManageBulkRegistrationController.php
+++ app/Http/Controllers/ManageBulkRegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Album;
 use App\Models\Artist;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use SplFileObject;
 
@@ -123,5 +124,124 @@ class ManageBulkRegistrationController extends Controller
         }
 
         return redirect()->route('manage.bulk.regist.index')->with('message', 'CSV登録が完了しました。');
+    }
+
+    public function googlespreadsheet(Request $request)
+    {
+        $validator = \Validator::make(
+            $request->all(),
+            [
+                'url' => [
+                    'required',
+                    'active_url',
+                    'regex' => '/^https:\/\/docs\.google\.com\/spreadsheets\/.+/$',
+                ],
+            ],
+            [],
+            [
+                'url' => 'Googleスプレッドシート URL',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        try {
+            \DB::transaction(function () use($request) {
+                $matches = null;
+                preg_match('/^https:\/\/docs\.google\.com\/spreadsheets\/d\/{.+}\/edit/$', $matches);
+                dd($matches);
+
+                $client = new Client([
+                    'base_url' => '',
+                ]);
+
+
+                \setlocale(LC_ALL, 'ja_JP.UTF-8');
+
+                $uploaded_file = $request->file('csv_file');
+
+                $file_path = $request->file('csv_file')->path($uploaded_file);
+
+                $file = new SplFileObject($file_path);
+                $file->setFlags(SplFileObject::READ_CSV);
+
+                $row_count = 1;
+                foreach ($file as $row_data) {
+                    if ($row_count > 1) {
+                        $row = collect($row_data);
+                        // $row->transform(function($item, $key) {
+                        //     return mb_convert_encoding($item, 'UTF-8', 'SJIS');
+                        // });
+
+                        // アルバムアーティスト
+                        $album_artist = Artist::where('name', $row->get(1))->first();
+                        if (!$album_artist) {
+                            $album_artist = Artist::create([
+                                'name' => $row->get(1),
+                                'belonging' => '',
+                            ]);
+                        }
+
+                        // アルバム
+                        $album = Album::where('title', $row->get(0))
+                            ->where('artist_id', $album_artist->id)
+                            ->first();
+                        if (!$album) {
+                            $album = Album::create([
+                                'title' => $row->get(0),
+                                'artist_id' => $album_artist->id,
+                                'artist_name' => $row->get(2),
+                                'description' => '',
+                            ]);
+                        }
+
+                        // 楽曲
+                        $music = $album->musics()->where('track_no', $row->get(3))->first();
+                        if (!$music) {
+                            $music = $album->musics()->create([
+                                'title' => $row->get(4),
+                                'track_no' => $row->get(3),
+                            ]);
+                        }
+
+                        // パートアーティスト
+                        $part_artist = Artist::where('name', $row->get(6))->first();
+                        if (!$part_artist) {
+                            $part_artist = Artist::create([
+                                'name' => $row->get(6),
+                                'belonging' => '',
+                            ]);
+                        }
+
+                        // パート
+                        $part = Part::where('artist_id', $part_artist->id)
+                            ->where('artist_name', $row->get(7))
+                            ->where('part_name', $row->get(5))
+                            ->first();
+                        if (!$part) {
+                            $music->parts()->create([
+                                'artist_id' => $part_artist->id,
+                                'artist_name' => $row->get(7),
+                                'part_name' => $row->get(5),
+                            ]);
+                        }
+                    }
+
+                    $row_count++;
+                }
+            });
+        } catch(\Exception $e) {
+            $validator->after(function($validator) {
+                $validator->errors()->add('csv_file', 'データの登録に失敗しました。');
+            });
+        }
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        return redirect()->route('manage.bulk.regist.index')->with('message', 'Google スプレッドシートの登録が完了しました。');
     }
 }
--- resources/views/manage/bulk/regist/index.blade.php
+++ resources/views/manage/bulk/regist/index.blade.php
@@ -20,6 +20,16 @@
                         </div>
                         {{ Form::submit('送信', ['class'=>'form-control']) }}
                     {{ Form::close() }}
+
+                    <h3>Googleスプレッドシート</h3>
+                    {{ Form::open(['route'=>['manage.bulk.regist.gss'], 'method'=>'POST', 'class'=>'form-inline']) }}
+                        <div class="form-group">
+                        {{ Form::text('url', null, ['class'=>'form-control-file'.ViewUtil::hasErrorClass($errors, 'url'), 'placeholder'=>'https://docs.google.com/spreadsheets/d/XXXXXXXXXX/edit?usp=sharing']) }}
+                        <p class="help-block">※GoogleスプレッドシートのURLを貼り付けてください。</p>
+                        @include('layouts.parts.error_message', ['key'=>'url'])
+                        </div>
+                        {{ Form::submit('送信', ['class'=>'form-control']) }}
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>

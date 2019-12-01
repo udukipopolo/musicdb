58f70c052d3bf7fbbccd56f2542df162f6043f92
udukipopolo <udukipopolo@gmail.com>
2019-12-01 11:07:57 +0900

    bulk

--- resources/views/manage/bulk/regist/index.blade.php
+++ resources/views/manage/bulk/regist/index.blade.php
@@ -15,7 +15,7 @@
                     {{ Form::open(['route'=>['manage.bulk.regist.csv'], 'method'=>'POST', 'class'=>'form-inline', 'files'=>true]) }}
                         <div class="form-group">
                         {{ Form::file('csv_file', ['class'=>'form-control-file'.ViewUtil::hasErrorClass($errors, 'csv_file')]) }}
-                        <p class="help-block">※文字エンコーディングはUTF-8にしてください。</p>
+                        <p class="help-block">※文字エンコーディングはUTF-8にしてください。</p><br>
                         <p class="help-block">サンプルは<a href="/sample/音楽DB一括登録用フォーマット-サンプル.csv" target="_blank">こちら</a></p>
                         @include('layouts.parts.error_message', ['key'=>'csv_file'])
                         </div>
@@ -26,8 +26,8 @@
                     {{ Form::open(['route'=>['manage.bulk.regist.gss'], 'method'=>'POST', 'class'=>'form-inline']) }}
                         <div class="form-group">
                         {{ Form::text('url', null, ['class'=>'form-control-file'.ViewUtil::hasErrorClass($errors, 'url'), 'placeholder'=>'https://docs.google.com/spreadsheets/d/XXXXXXXXXX/edit?usp=sharing']) }}
-                        <p class="help-block">※GoogleスプレッドシートのURLを貼り付けてください。</p>
-                        <p class="help-block">※共有で、リンクを知っている全員が編集または閲覧可にしてください。</p>
+                        <p class="help-block">※GoogleスプレッドシートのURLを貼り付けてください。</p><br>
+                        <p class="help-block">※共有で、リンクを知っている全員が編集または閲覧可にしてください。</p><br>
                         <p class="help-block">サンプルは<a href="https://docs.google.com/spreadsheets/d/1yQ2Q-hUYfuE1HRKCE7Jj-PHlFn41uwd1yX-EjKH0Yo0/edit#gid=0" target="_blank">こちら</a></p>
                         @include('layouts.parts.error_message', ['key'=>'url'])
                         </div>

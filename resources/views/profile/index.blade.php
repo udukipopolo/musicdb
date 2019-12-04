774869a83abc451e0daf42f9b15fd93a238ae970
udukipopolo <udukipopolo@gmail.com>
2019-12-04 12:56:32 +0900

    プロフィール編集

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,7 +11,7 @@
                     <div class="row">
                         @include('layouts.parts.message')
 
-                        <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                        <div class="col-md-6 offset-md-3 col-sm-8 offset-sm-2">
                             {{ Html::linkRoute('profile.edit', 'プロフィール編集', [], ['class'=>'btn btn-light btn-block']) }}
                         </div>
                     </div>

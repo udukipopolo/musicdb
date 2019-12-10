892a7782ae45e092d070482314fb729aa285d735
udukipopolo <udukipopolo@gmail.com>
2019-12-10 17:27:52 +0900

    多言語対応

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -1,10 +1,11 @@
 <?php
 return [
-    'search' => '検索',
     'login' => 'ログイン',
     'logout' => 'ログアウト',
     'profile' => 'プロフィール',
     'regist' => '登録',
     'update' => '更新',
     'register' => '会員登録',
+    'search' => '検索',
+    'management' => '管理',
 ];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -110,7 +110,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    管理 <span class="caret"></span>
+                                    @lang('messages.management') <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     {{ Html::linkRoute('manage.album.index', 'アルバム管理', [], ['class'=>'dropdown-item']) }}
@@ -124,7 +124,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    {{ Html::linkRoute('profile.index', 'プロフィール編集', [], ['class'=>'dropdown-item']) }}
+                                    {{ Html::linkRoute('profile.index', __('messages.profile'), [], ['class'=>'dropdown-item']) }}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">

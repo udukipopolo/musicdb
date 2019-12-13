928fcf105943f1d95684b83d3f055f92940dea95
udukipopolo <udukipopolo@gmail.com>
2019-12-13 10:55:43 +0900

    多言語対応

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\DebugbarView::class,
+            \App\Http\Middleware\CheckLocale::class,
         ],
 
         'api' => [

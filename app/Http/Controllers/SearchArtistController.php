5aedbe0183ae4f481d7da23ff1d87be3a5ef7d9e
udukipopolo <udukipopolo@gmail.com>
2019-12-19 15:00:01 +0900

    アーティスト検索

--- app/Http/Controllers/SearchArtistController.php
+++ app/Http/Controllers/SearchArtistController.php
@@ -29,7 +29,7 @@ class SearchArtistController extends Controller
                     $artists->groupBy('artists.id');
                     $artists->orderBy('artists.name', 'ASC');
                 } else {
-                    $artists->addSelect(\DB::raw("MAX(MATCH(locale_names.name) AGAINST( ?  IN NATURAL LANGUAGE MODE)) AS score", [$request->artist_name]));
+                    $artists->addSelectRaw("MAX(MATCH(locale_names.name) AGAINST( ?  IN NATURAL LANGUAGE MODE)) AS score", [$request->artist_name]);
                     $artists->whereRaw("MATCH(locale_names.name) AGAINST( ?  IN NATURAL LANGUAGE MODE)", [$request->artist_name]);
                     $artists->groupBy('artists.id');
                     $artists->orderBy('score', 'DESC');

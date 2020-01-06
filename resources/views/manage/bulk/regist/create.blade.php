857e7f9387d3756518838a9b10b16b9b3bb469a5
udukipopolo <udukipopolo@gmail.com>
2020-01-06 13:34:35 +0900

    一括登録

--- resources/views/manage/bulk/regist/create.blade.php
+++ resources/views/manage/bulk/regist/create.blade.php
@@ -6,19 +6,20 @@
 <script>
 $(function() {
     var data = [
-    ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
-    ['2017', 10, 11, 12, 13],
-    ['2018', 20, 11, 14, 13],
-    ['2019', 30, 15, 12, 13]
+        ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
+        ['2017', 10, 11, 12, 13],
+        ['2018', 20, 11, 14, 13],
+        ['2019', 30, 15, 12, 13]
     ];
 
     var container = document.getElementById('sheet');
     var hot = new Handsontable(container, {
-    data: data,
-    rowHeaders: true,
-    colHeaders: true,
-    filters: true,
-    dropdownMenu: true
+        data: data,
+        rowHeaders: true,
+        colHeaders: true,
+        filters: true,
+        dropdownMenu: true,
+        allowInsertRow: true
     });
 });
 </script>

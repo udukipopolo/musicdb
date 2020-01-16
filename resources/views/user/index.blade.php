d6068e575fce9181489bb2480cd3e07342c8f948
udukipopolo <udukipopolo@gmail.com>
2020-01-16 11:10:13 +0900

    デザイン修正

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -39,7 +39,7 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-12 mb-3">
                             <table class="table table-bordred table-striped">
                                 <thead>
                                     <tr>

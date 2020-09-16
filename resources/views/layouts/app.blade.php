e84741a396ab2a08b893fcad2fe99c916c1c5d2e
Takuya Murakami <udukipopolo@gmail.com>
2020-09-16 22:06:44 +0900

    会員登録メニュー非表示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,11 +63,13 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('messages.login') }}</a>
                             </li>
+                            {{--
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('messages.register') }}</a>
                                 </li>
                             @endif
+                            --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
